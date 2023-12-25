14b04a3e48996d2e0a669955d2504b477ba0981e
Phillip Lu <Plu2978@conestogac.on.ca>
2023-12-25 01:28:25 -0500

    removed prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,5 @@
 module.exports = {
   root: true,
-  extends: ['plugin:@next/next/recommended', '@payloadcms', 'plugin:prettier/recommended'],
+  extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
-  plugins: ['prettier'],
-  rules: {
-    "prettier/prettier": "error",
-  },
 }
